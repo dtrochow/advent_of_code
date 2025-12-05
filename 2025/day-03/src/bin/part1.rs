f023706ec0f368c00d4ef5045b26504ab602e566
Dominik Trochowski <dominik.trochowski@gmail.com>
2025-12-05 17:38:20 +0100

    Solved Day3 2025 Part1

---
+++ 2025/day-03/src/bin/part1.rs
@@ -0,0 +1,60 @@
+use aoc_lib::read_lines;
+
+type BatteryBank = Vec<u32>;
+
+fn get_battery_banks(lines: Vec<String>) -> Vec<BatteryBank> {
+    let mut battery_banks: Vec<BatteryBank> = Vec::new();
+    for line in lines {
+        battery_banks.push(line.chars().map(|b| b.to_digit(10).unwrap()).collect());
+    }
+    battery_banks
+}
+
+fn find_tens_digit(battery_bank: &BatteryBank) -> (u32, Vec<usize>) {
+    let mut max = 0;
+    for i in (0..(battery_bank.len() - 1)).rev() {
+        if battery_bank[i] > max {
+            max = battery_bank[i];
+        }
+    }
+
+    let mut indexes: Vec<usize> = Vec::new();
+    for (i, bank) in battery_bank.iter().enumerate() {
+        if *bank == max {
+            indexes.push(i);
+        }
+    }
+    (max, indexes)
+}
+
+fn find_max_units_digit(battery_bank: &BatteryBank, tens_indexes: Vec<usize>) -> u32 {
+    let mut max = 0;
+    for tens_index in tens_indexes {
+        for bank in battery_bank.iter().skip(tens_index + 1) {
+            if *bank > max {
+                max = *bank;
+            }
+        }
+    }
+    max
+}
+
+fn get_largest_joltage(battery_bank: &BatteryBank) -> u32 {
+    let (tens, tens_indexes) = find_tens_digit(battery_bank);
+    let units = find_max_units_digit(battery_bank, tens_indexes);
+
+    10 * tens + units
+}
+
+fn main() {
+    let lines = read_lines("./src/bin/input1.txt");
+
+    let battery_banks: Vec<BatteryBank> = get_battery_banks(lines);
+
+    let mut joltages_sum: u32 = 0;
+    for battery_bank in battery_banks {
+        joltages_sum += get_largest_joltage(&battery_bank);
+    }
+
+    println!("Maximum joltage: {}", joltages_sum);
+}
